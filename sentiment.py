a9e28216e81b07c8b01d7b8572830c18e4fa438b
Nidhish <55269918+nidhishs@users.noreply.github.com>
2021-01-31 21:23:41 +0100

    Added lemmatization to text processing function.
    The clean_text function was renamed to process_text. Stop words
    are removed from the text and remaining words are lemmatized.

--- sentiment.py
+++ sentiment.py
@@ -1,10 +1,10 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import re
+import spacy
 
 
-def clean_text(text):
+def process_text(text):
     hashtags = re.compile(r"^#\S+|\s#\S+")
     mentions = re.compile(r"^@\S+|\s@\S+")
     urls = re.compile(r"(https|http)?://\S+")
@@ -17,6 +17,12 @@ def clean_text(text):
     # Fix word lengthening. Most words in english have a maximum of two repeated characters.
     text = re.sub(r"(.)\1{2,}", r"\1\1", text)
     # Convert entire text to lowercase and remove redundant whitespaces.
-    text = " ".join(text.lower().split())
+    cleaned_text = " ".join(text.lower().split())
 
-    return text
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(cleaned_text)
+
+    # Lemmatise words from original text that are not stopwords.
+    processed_text = " ".join([token.lemma_ for token in doc if not token.is_stop])
+
+    return processed_text
